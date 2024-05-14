de8bac5d59cf9f0b494fa7df06f1204c2e3489ff
Simon Niedermayr <simon.niedermayr@tum.de>
2024-05-14 14:01:22 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # v4Dv - Interactive 4D volume visualization
 
 ToDo
+
+## Installation
+
+with pip
+```
+pip install git+ssh://git@github.com/KeKsBoTer/v4dv.git@master
+```
+
+## Usage
+
+**Viewer** (jupyter notebook)
+```python
+from v4dv import viewer
+import numpy as np
+from matplotlib import pyplot as plt
+
+viewer(
+    #[T,C,D,W,H]
+    np.random.rand(1,1,32,32,32),
+    plt.get_cmap("viridis"),   
+)
+```
+
+**Render Image**
+```python
+from v4dv import felix_cmap_hack, render
+
+colormap = felix_cmap_hack(plt.get_cmap("magma"))
+img = render(
+        # [T,D,H,W]
+        np.random.rand(2,32,32,32),
+        colormap,
+        0.5, # timestep
+        width=1024,
+        height=1024,
+    )
+
+```
+
--- test/render.py
+++ test/render.py
@@ -1,38 +1,13 @@
 from matplotlib import pyplot as plt
 from PIL import Image
-from matplotlib.colors import Colormap, LinearSegmentedColormap, ListedColormap
-import v4dv
 import numpy as np
 
-
-def felix_cmap_hack(cmap: Colormap) -> Colormap:
-    """changes the alpha channel of a colormap to be diverging (0->1, 0.5 > 0, 1->1)
-
-    Args:
-        cmap (Colormap): colormap
-
-    Returns:
-        Colormap: new colormap
-    """
-    cmap = cmap.copy()
-    if isinstance(cmap, ListedColormap):
-        cmap.colors = cmap.colors.copy()
-        for i, a in enumerate(cmap.colors):
-            a.append(2 * abs(i / cmap.N - 0.5))
-    elif isinstance(cmap, LinearSegmentedColormap):
-        cmap._segmentdata["alpha"] = np.array(
-            [[0.0, 1.0, 1.0], [0.5, 0.0, 0.0], [1.0, 1.0, 1.0]]
-        )
-    else:
-        raise TypeError(
-            "cmap must be either a ListedColormap or a LinearSegmentedColormap"
-        )
-    return cmap
+from v4dv import felix_cmap_hack, render
 
 
 colormap = felix_cmap_hack(plt.get_cmap("magma"))
 img = (
-    v4dv.render(
+    render(
         np.load("volumes/diffusion_trj.npz")["trj"][0, :1],
         colormap,
         0.5,
